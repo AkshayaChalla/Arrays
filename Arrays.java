0592ff14992326f6dd6eccda49227e37751a7cd8
AkshayaChalla <akshaya.challa215@gmail.com>
2025-01-28 10:24:00 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int size = 5;
+        int[] array = new int[size];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+        System.out.println("elemnts osf 1D array:");
+        for(int i = 0; i<=array.length;i++){
+            System.out.println(array[i] + " ");
+        }
+    }
+    
+}
